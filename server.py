cd2787df493f032a0cbb0b94d20f36fa947692cc
07051999 <40885724+07051999@users.noreply.github.com>
2018-07-06 14:23:50 +0530

    Create server.py

---
+++ server.py
@@ -0,0 +1,10 @@
+# networklab
+import socket
+UDP_IP_ADDRESS="10.1.24.128"
+UDP_PORT_NO=6789
+serverSock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+serverSock.bind((UDP_IP_ADDRESS,UDP_PORT_NO))
+print("server waiting")
+while True:
+ data,addr=serverSock.recvfrom(1024)
+ print("message",data)
